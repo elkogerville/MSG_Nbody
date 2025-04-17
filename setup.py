dad159417df563cd557b52090c4061351bb41198
elkogerville <elkogerville@gmail.com>
2025-04-17 21:48:57 +0200

    restructured analysis.py

--- setup.py
+++ setup.py
@@ -2,16 +2,25 @@ from setuptools import setup, find_packages
 
 setup(
     name='MSG_Nbody',
-    version='0.2.1',
+    version='0.2.2',
     description='Nbody simulation code for galaxy interactions',
     url='https://github.com/elkogerville/MSG_Nbody',
     author='Elko Gerville-Reache',
     author_email='elkogerville@gmail.com',
     license='MIT',
-    long_description=open('README.md').read(),
+    long_description=open('README_MSG.md').read(),
     long_description_content_type='text/markdown',
     include_package_data=True,
-    packages=find_packages(),
+    packages=find_packages(exclude=[
+        'ANIMATIONS*', 
+        'DOCUMENTATION*', 
+        'Initial_Conditions*', 
+        'Tests*',
+	'MSG_Nbody.egg-info*',
+        '*.egg-info*',
+        'build*',
+        'dist*',
+    ]),
     install_requires=[
         'numpy',
         'numba',
