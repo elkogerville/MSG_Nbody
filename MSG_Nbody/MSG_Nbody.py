9a5e6ec4a4da33757429c70644b4be47604aad5d
elkogerville <elkogerville@gmail.com>
2025-04-24 15:01:24 +0200

    fixed MSG_Nbody engine

--- MSG_Nbody/MSG_Nbody.py
+++ MSG_Nbody/MSG_Nbody.py
@@ -13,6 +13,7 @@ Dependencies:
 import os
 import numpy as np
 from tqdm import tqdm
+from numba import njit
 from .simulation_setup import ascontiguousarray
 from .acceleration_potential import compute_accel_potential
 from .input_output import save_snapshot_2_binary_file, create_output_directory
@@ -50,6 +51,10 @@ def MSG_Nbody(positions, velocities, masses, dt, timesteps, **kwargs):
         last saved timestep. ex: if start_idx = 2000, the simulation will start
         from timestep 2001. the initial conditions passed into MSG_Nbody should
         be the positions and velocities of the particles at timestep 2000
+    directory: str
+        path to computation directory. by default is automatically created
+        but it can be useful to pass in an existing directory to resume
+        a simulation. if resuming a simulation, remember to set a start_idx
     block_size: int
         size of the blocks used for batch processing of interactions.
         if N < block_size, the algorithm degrades to processing the
@@ -65,13 +70,12 @@ def MSG_Nbody(positions, velocities, masses, dt, timesteps, **kwargs):
     # compute number of particles
     N = positions.shape[0]
     # enforce appropriate array dimensions
-    # if (positions.shape != (N, 3)) or (velocities.shape != (N, 3)) or (masses.shape != (N, 1)):
     if (positions.shape != (N, 3)) or \
        (velocities.shape != (N, 3)) or \
        (masses.shape != (N, 1)):
         error_message = (
-            "ERROR: Ensure 'gal_pos' and 'gal_vel' have shape (N, 3), "
-            "and 'mass' has shape (N, 1), where N is the number of particles.\n"
+            "ERROR: Ensure 'positions' and 'velocities' have shapes (N, 3), "
+            "and 'masses' has shape (N, 1), where N is the number of particles.\n"
             f"provided shapes -> positions: {positions.shape}, "
             f"velocities: {velocities.shape}, masses: {masses.shape}\n"
             r"/ᐠ_ ꞈ _ᐟ\ <(Fix it...)"
@@ -85,16 +89,35 @@ def MSG_Nbody(positions, velocities, masses, dt, timesteps, **kwargs):
     start_idx = 1
     if 'start_idx' in kwargs:
         start_idx = int(kwargs['start_idx'])
+    if 'directory' in kwargs and os.path.isdir(kwargs['directory']):
+        if start_idx == 1:
+            print(
+                "\nWARNING: you have not set a value for 'start_idx' "
+                'while providing a directory path.\n'
+                'WARNING: this can overwrite simulation snapshots!\n'
+                'by default, start_idx is set to 1 for a new simulation.\n'
+                'if you are trying to resume a simulation\n'
+                'set start_idx to the last completed timestep.\n'
+                'ex:\n'
+                "data = np.load('snapshot_N100000_timestep_4120.npy')\n"
+                'pos, vel = data[:,0:3], data[:,3:6]\n'
+                'MSG_Nbody(pos, vel, masses, dt, 5000, directory=directory, start_idx=4120)'
+            )
+            while True:
+                confirm = input("\n\ntype 'yes' or 'y' to proceed: ")
+                if confirm.lower() in ['y', 'yes']:
+                    break
+                else:
+                    raise ValueError("aborted: confirmation not received")
+        directory = kwargs['directory']
+    else:
+        directory = create_output_directory(N)
     # batch processing block size
     block_size = 3000
     if 'block_size' in kwargs:
         block_size = int(kwargs['block_size'])
     # ensure integer amount of timesteps
     timesteps = int(timesteps)
-    if 'directory' in kwargs and os.path.isdir(kwargs['directory']):
-        directory = kwargs['directory']
-    else:
-        directory = create_output_directory(N)
 
     # compute softening length based on Dehnen et al., 2001
     softening = 0.017 * ( (N/1e5) )**(-0.23)
@@ -123,10 +146,12 @@ def MSG_Nbody(positions, velocities, masses, dt, timesteps, **kwargs):
     print(r'simulation running....  /ᐠ –ꞈ –ᐟ\<[pls be patient]')
     for i in tqdm(range(start_idx, timesteps+1)):
         # 1/2 kick
-        velocities += accel * dt/2.0
+        # velocities += accel * dt/2.0
+        velocities = leapfrog_integration(velocities, accel, dt, 2.0)
 
         # drift
-        positions += velocities * dt
+        # positions += velocities * dt
+        positions = leapfrog_integration(positions, velocities, dt, 1.0)
 
         # update accelerations
         accel, potential = compute_accel_potential(positions, masses,
@@ -134,11 +159,17 @@ def MSG_Nbody(positions, velocities, masses, dt, timesteps, **kwargs):
                                                    softening_sq, N,
                                                    block_size=block_size)
         # update velocities
-        velocities += accel * dt/2.0
+        #velocities += accel * dt/2.0
+        velocities = leapfrog_integration(velocities, accel, dt, 2.0)
 
         # write positions, velocities, and potential to binary file
         if i % snapshot_save_rate == 0:
             save_snapshot_2_binary_file(sim_snapshot, positions, velocities,
                                         potential, directory, N, i)
 
-    return r'simulation complete [yay!!! =＾● ᆺ ●＾= ✿✧･ﾟ: ✧･ﾟ]'
+    print(r'simulation complete [yay!!! =＾● ᆺ ●＾= ✿✧･ﾟ: ✧･ﾟ]')
+
+@njit(parallel=True, fastmath={'nnan', 'ninf'}, inline='always')
+def leapfrog_integration(quantity, rate_of_change, dt, factor):
+    quantity += rate_of_change * dt/factor
+    return quantity
